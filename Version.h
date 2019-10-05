5ec94746ce1fcef966f1e5ad42abff6e873ec9d6
William Nichols <wcomhelp@users.noreply.github.com>
2019-10-05 01:27:23 -0600

    Update Version

--- Version.h
+++ Version.h
@@ -1,9 +1,9 @@
 #pragma once
-#define FILEVER 4,3,0,3111
+#define FILEVER 4,3,1,3321
 #define PRODUCTVER FILEVER
-#define STRFILEVER "4.3.0.3111"
+#define STRFILEVER "4.3.1.3321"
 #define STRPRODUCTVER STRFILEVER
-#define APPVERSION L"4.3.0"
-#define RAINMETER_VERSION ((4 * 1000000) + (3 * 1000) + 0)
-const int revision_number = 3111;
-const bool revision_beta = true;
+#define APPVERSION L"4.3.1"
+#define RAINMETER_VERSION ((4 * 1000000) + (3 * 1000) + 1)
+const int revision_number = 3321;
+const bool revision_beta = false;
